cb88b7e158977e85b3fe367c753249621f53c21e
Gadzooksgaming <102859034+Gadzooksgaming@users.noreply.github.com>
2022-12-06 18:18:06 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+var SpeechRecognition=window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+
+function start()
+{
+    document.getElementById("textbox").innerHTML="";
+    recognition.start();
+
+    
+}
+recognition.onresult = function(event){
+        console.log(event);
+        var Content=event.results[0][0].transcript;
+        console.log(Content);
+        document.getElementById("textbox").innerHTML=Content;
+        speak();
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+    Speak_data = document.getElementById("textbox").value;
+    var utterThis=new SpeechSynthesisUtterance(Speak_data);
+    synth.speak(utterThis);
+    Webcam.attach(camera);
+}
+
+Webcam.set({
+    width:350,
+    height:250,
+    image_format:'png',
+    png_quality:100
+});
+camera=document.getElementById("webcam");
